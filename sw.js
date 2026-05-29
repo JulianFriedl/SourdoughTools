6bfcd736973e0c8058f35bbbd0f432d6032a4524
Julian Friedl <julian.friedl@student.tugraz.at>
2026-05-29 11:06:06 +0200

    Added initial tools

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'sourdough-v1';
+const ASSETS = ['./sourdough_temp_tools.html', './manifest.json', './sw.js'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
